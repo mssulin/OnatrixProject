912d079de5e4bddd0346387131286d3b9ef137f9
msulinx <msulin0329@hotmail.com>
2025-10-24 23:36:32 +0200

    Service details page added and half done

--- OnatrixProject/tailwind.config.js
+++ OnatrixProject/tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   content: [
     "./Views/**/*.cshtml",
     "./Views/Partials/**/*.cshtml",
-    "./wwwroot/js/**/*.js"
+    "./wwwroot/js/**/*.js",
+    "./App_Plugins/OnatrixRte/**/*.json" 
+  ],
+  safelist: [ 
+    'text-primary',
+    'text-secondary',
+    'text-body-text',
+    'text-body-100',
+    'text-rte-xl',
+    'text-rte-lg',
+    'text-rte-md',
+    'text-rte-sm',
+    'text-rte-xs',
+    'font-light',
+    'font-normal',
+    'font-bold',
+    'rte-ul'
   ],
   theme: {
     extend: {
@@ -12,14 +28,21 @@ module.exports = {
         secondary: "var(--color-secondary)",
         white: "var(--color-white)",
         black: "var(--color-black)",
-        "white-100" : "var(--color-white-100)",
+        "white-100": "var(--color-white-100)",
         "body-text": "var(--color-body-text)",
-        "border-grey": "var(--color-grey)",
+        "border-gray": "var(--color-border-gray)"
       },
-    fontFamily: {
-      poppins: ['Poppins', 'sans-serif'],
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+      fontSize: {
+        'rte-xl': 'var(--text-rte-xl)',
+        'rte-lg': 'var(--text-rte-lg)',
+        'rte-md': 'var(--text-rte-md)',
+        'rte-sm': 'var(--text-rte-sm)',
+        'rte-xs': 'var(--text-rte-xs)'
+      }
     }
-    } 
-    },
+  },
   plugins: []
-}
+};
