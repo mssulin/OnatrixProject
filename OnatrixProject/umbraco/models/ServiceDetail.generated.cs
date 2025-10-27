bba0bc105f7dd9c0f1eddccebb04e3f70cb70a3e
msulinx <msulin0329@hotmail.com>
2025-10-27 13:43:33 +0100

    Design and logic for contact info added. Logic for Partner section and topbar fixed

--- OnatrixProject/umbraco/models/ServiceDetail.generated.cs
+++ OnatrixProject/umbraco/models/ServiceDetail.generated.cs
@@ -74,6 +74,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel ServiceDetailsAsides => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "serviceDetailsAsides");
 
 		///<summary>
+		/// Section
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("serviceDetailSection")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel ServiceDetailSection => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "serviceDetailSection");
+
+		///<summary>
 		/// Thumbnail
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
